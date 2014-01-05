5fde7f9d7fd60da6650406a7d3d03d2e4aaca623
Ty Richards <me@tyrichards.com>
2014-01-04 20:04:58 -0600

    Set Index Page to be root

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Eleven::Application.routes.draw do
   resources :pins
 
   devise_for :users
-  root "pins#index"
+  # root "pins#index"
+  root "pages#index"
   get "about" => "pages#about" # creates about path
 
   # The priority is based upon order of creation: first created -> highest priority.
