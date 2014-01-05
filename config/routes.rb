0dc71efb32adc57ee499c2f7bdd71e4d193f1c90
Ty Richards <me@tyrichards.com>
2014-01-04 21:18:28 -0600

    Create sign up screen as initial page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Eleven::Application.routes.draw do
   devise_for :users
   root "pins#index"
   get "about" => "pages#about" # creates about path
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
